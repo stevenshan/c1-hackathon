cd53ec9af1d9b8e72eb297ba4136de1ea53ab787
2016asuri <ananyas@andrew.cmu.edu>
2018-08-16 14:00:07 -0400

    fix sort by ratings

--- charity-navigator/find_charity.py
+++ charity-navigator/find_charity.py
@@ -7,6 +7,7 @@ app_id = "267c5c0d"
 app_key = "95a6143616abcad971c5966409b0cb52"
 my_profile = {}
 
+# return list of dictionaries of charities in same zip code
 def get_local_charities(zip_code):
 	results = {}
 
@@ -16,22 +17,22 @@ def get_local_charities(zip_code):
 	result = r.json()
 	for org in result:
 		if not('errorMessage' in org.keys()):
-			results[org['charityName']] = org
+			results.append(org)
 	return results
 
-def get_names(charities):
-	return list(charities.keys())
+# return list of names of charities in charity_list
+def get_names(charity_list):
+	return [c['charityName'] for c in charity_list]
 
 
-#def get_classification(charity):
-# 	return charity['irsClassification']['nteeType']
-
+# return list of dictionaries for each category from API
 def get_categories():
 	payload = {"app_id": app_id, "app_key": app_key}
 	url = "https://api.data.charitynavigator.org/v2/Categories"
 	r = requests.get(url, params=payload)
 	return r.json()
 
+# return dictionary of dictionaries for each cause from API
 def get_causes(categories):
 	causes = {}
 	for cat in categories:
@@ -40,55 +41,60 @@ def get_causes(categories):
 	return causes
 
 
-# get suggestions based on location, interests, ratings
+# get list of suggestions based on location, interests
 def get_suggestions_by_zip(zip_code, interests):
 	causes = get_causes(get_categories())
 	results = {}
 	for interest in interests:
 		cause_id = causes[interest]['causeID']
-		payload = {"app_id": app_id, "app_key": app_key, "zip": zip_code, "causeID": cause_id, "rating": "RATING: DESC"}
+		payload = {"app_id": app_id, "app_key": app_key, "zip": zip_code, "causeID": cause_id}
 		url = "https://api.data.charitynavigator.org/v2/Organizations"
 		r = requests.get(url, params=payload)
 		result = r.json()
 		for org in result:
-			if not('errorMessage' in org.keys()):
+			if not(type(org) is str) and not('errorMessage' in org.keys()):
 				results[org['charityName']] = org
-	return results
+	return list(results.values())
 
-# get suggestions based on location, interests, ratings
+# get list of suggestions based on location, interests
 def get_suggestions_by_city(city, interests):
 	causes = get_causes(get_categories())
 	results = {}
 	for interest in interests:
 		cause_id = causes[interest]['causeID']
-		payload = {"app_id": app_id, "app_key": app_key, "city": city, "causeID": cause_id, "rating": "RATING: DESC"}
+		payload = {"app_id": app_id, "app_key": app_key, "city": city, "causeID": cause_id}
 		url = "https://api.data.charitynavigator.org/v2/Organizations"
 		r = requests.get(url, params=payload)
 		result = r.json()
 		for org in result:
-			if not('errorMessage' in org.keys()):
+			if not(type(org) is str) and not('errorMessage' in org.keys()):
 				results[org['charityName']] = org
-	return results
+	return list(results.values())
 
-# get suggestions based on location, interests, ratings
+# get list of suggestions based on location, interests
 def get_suggestions_by_state(state, interests):
 	causes = get_causes(get_categories())
 	results = {}
 	for interest in interests:
 		cause_id = causes[interest]['causeID']
-		payload = {"app_id": app_id, "app_key": app_key, "state": state, "causeID": cause_id, "rating": "RATING: DESC"}
+		payload = {"app_id": app_id, "app_key": app_key, "state": state, "causeID": cause_id}
 		url = "https://api.data.charitynavigator.org/v2/Organizations"
 		r = requests.get(url, params=payload)
 		result = r.json()
 		for org in result:
-			if not('errorMessage' in org.keys()):
+			if not(type(org) is str) and not('errorMessage' in org.keys()):
 				results[org['charityName']] = org
-	return results
+	return list(results.values())
 
+# sort charity_list in order of descending ratings 
+def sort_by_ratings(charity_list):
+	charity_list = [c for c in charity_list if 'currentRating' in c.keys()]
+	return sorted(charity_list, key = lambda charity: charity['currentRating']['rating'], reverse=True)
 
-def get_ratings(charities):
-	return [c_dict['currentRating']['rating'] for c_dict in charity_list.values()]
+# returns list of ratings for each charity
+def get_ratings(charity_list):
+	return [c_dict['currentRating']['rating'] for c_dict in charity_list]
 
-pprint.pprint(get_names(get_suggestions_by_zip(my_zip_code, ['Diseases, Disorders, and Disciplines', 'Medical Research'])))
-pprint.pprint(get_names(get_suggestions_by_city('McLean', ['Diseases, Disorders, and Disciplines', 'Medical Research'])))
-pprint.pprint(get_names(get_suggestions_by_state('VA', ['Diseases, Disorders, and Disciplines', 'Medical Research'])))
+pprint.pprint(get_names(sort_by_ratings(get_suggestions_by_zip(my_zip_code, ['Diseases, Disorders, and Disciplines', 'Medical Research']))))
+pprint.pprint(get_names(sort_by_ratings(get_suggestions_by_city('McLean', ['Diseases, Disorders, and Disciplines', 'Medical Research']))))
+pprint.pprint(get_names(sort_by_ratings(get_suggestions_by_state('VA', ['Diseases, Disorders, and Disciplines', 'Medical Research']))))
