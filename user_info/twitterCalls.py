21d3f2d464970abff9fe994322c7dd6ed98802b7
Kassidy Webb <ktwebb@crimson.ua.edu>
2018-08-16 17:58:37 -0400

    added twitter calls

---
+++ user_info/twitterCalls.py
@@ -0,0 +1,23 @@
+import tweepy
+
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+def main():
+  # Fill in the values noted in previous step here
+  cfg = {
+    "consumer_key"        : "YTjzYW8UDRUTNbStu2vSoOunv",
+    "consumer_secret"     : "3laraBw3oyMbqIt8RdAm281kKW4OpHukRSx90vXweFbM9ylvwN",
+    "access_token"        : "1030182022176796672-xpry2rloo4AxJdYMNnjlkLYiboafPY",
+    "access_token_secret" : "293ToO4YQDYERqQgSiNZLPB756IvBPoTlrmMb4c7t1eaC"
+    }
+
+  api = get_api(cfg)
+  tweet = "@Valentino_JD is having fun at Cap1SES!!!"
+  status = api.update_status(status=tweet)
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+  main()
