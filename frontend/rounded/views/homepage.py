7d5ae7027ff1b576a55651ceb430a3e33952812a
Steven Shan <steven.shan99@gmail.com>
2018-08-17 07:31:26 -0400

    added summary level data endpoint

--- frontend/rounded/views/homepage.py
+++ frontend/rounded/views/homepage.py
@@ -1,8 +1,23 @@
 from . import controller
-from rounded.core import url_tools
+from rounded.core import url_tools, user
+from rounded.core.user_info import get as BANK
 import flask
 
 @controller.route("/", methods=["GET"])
 def homepage():
     return flask.redirect(flask.url_for("app.setup"))
+
+@controller.route("/data", methods=["GET"])
+def datamessage():
+    accounts = BANK.ACCOUNTS
+    balance = "0.00"
+    for account in accounts:
+        if (account.get("nickname") == "Charity Account" and 
+                account.get("balance") != None):
+            balance = account.get("balance")
+            balance = "{0:.2f}".format(balance)   
+    return flask.jsonify({
+        "balance": balance,
+        "charity": user.getCharity(),
+    })
 
