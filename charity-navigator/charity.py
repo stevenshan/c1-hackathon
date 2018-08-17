2c697b3e02a5774972e36990e36266ea160cefff
Steven Shan <steven.shan99@gmail.com>
2018-08-17 15:12:44 -0400

    heroku deployment

--- charity-navigator/charity.py
+++
@@ -1,8 +0,0 @@
-class Charity:
-  def __init__(self, mydict):
-    self.dict = mydict
-    self.name = mydict['charityName']
-    self.id = mydict['ein']
-    self.cause = mydict['cause']['causeID']
-    self.mission = mydict['mission']
-    self.state = mydict['mailingAddress']['stateOrProvince']
