c1a4f7ef94c1fd27237f46805f360bc2d2d60785
Steven Shan <steven.shan99@gmail.com>
2018-08-16 22:30:11 -0400

    frontend voting screen

--- frontend/rounded/mod_voting/views/vote.py
+++ frontend/rounded/mod_voting/views/vote.py
@@ -1,24 +1,19 @@
 import flask
 from rounded.mod_voting import controller
+import json
 
-charities = ["Children's and Family Services",
-'Homeless Services',
-'Youth Development, Shelter, and Crisis Services',
-'Food Banks, Food Pantries, and Food Distribution',
-'Social Services',
-'Multipurpose Human Service Organizations',
-'Scholarship and Financial Support',
-'Private Liberal Arts Colleges',
-'Youth Education Programs and Services',
-'Education Policy and Reform',
-'Social and Public Policy Research']
+charities = [{'name': "Children's and Family Services", 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 817}, {'name': 'Homeless Services', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 519}, {'name': 'Youth Development, Shelter, and Crisis Services', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 243}, {'name': 'Food Banks, Food Pantries, and Food Distribution', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 882}, {'name': 'Social Services', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 634}, {'name': 'Multipurpose Human Service Organizations', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 224}, {'name': 'Scholarship and Financial Support', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 868}, {'name': 'Private Liberal Arts Colleges', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 69}, {'name': 'Youth Education Programs and Services', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 480}, {'name': 'Education Policy and Reform', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 993}, {'name': 'Social and Public Policy Research', 'description': "lorem ipsum delores something I don't feel like typing", 'votes': 335}]
 
-charities.sort() # by popularity
+charities.sort(key=lambda x: x["votes"], reverse=True) # by popularity
 
 @controller.route("/vote", methods=["GET"])
 def voting():
-    userBalance = 100.25
-    return flask.render_template("voting.html", firstCharity=charities[0], charities=charities[1:], userBalance=userBalance)
+    _charities = json.dumps(charities)
+    return flask.render_template(
+        "voting.html",
+        charities=charities,
+        _charities=_charities,
+    )
 
 # need leaderobard on left side
 # options to vote now or view other charities to vote for
